b7f22bd7bfa1f216cdcf26b2cb35de1069a3953c
jabborov077 <djabborovbehruz2@gmail.com>
2024-05-15 11:05:45 +0500

    oop

---
+++ massiv.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// const arr = [1, 2, 3, 4, 5, 6];
+
+// arr.forEach((element,index)=>{
+//     console.log(`${index}: ${element}`);
+// })
+
+// arr.pop()
+
+// console.log(arr);
+
+// arr.push()
+
+// console.log(arr);
+
+//  for (let i = 0; i < arr.length; i++) {
+//      console.log(arr[i]);
+// }
+
+
+// for (let element of arr){
+//     console.log(element);
+// }
+
+// arr [99] = 'malik'
+
+// console.log(arr.length);
+
+
+// arr.forEach(function (item, index,arr) {
+//     console.log(`${index}: ${item} massivimiz bu ${arr}`);
+// })
+
+// const arr = [1, 2, 3, 4, 5,23,414,42,6,44,76,24];
+
+// arr.sort(shuhratFn);
+// console.log(arr);
+// function shuhratFn(a,b){
+//     return a-b;
+// }
+
---
+++ spread.js
@@ -0,0 +1,36 @@
+'use strict'
+
+// function docket(x,y,z){
+//     console.log(x+y+z);
+// }
+
+// docket(1,2,3)
+
+// const number = [1,2,3,4,5]
+
+// function docket(x,y,z){
+//     console.log(x+y+z);
+// }
+
+// docket(...number)
+
+// const arr = [1,2,3,4,5]
+
+// const newArr = [...arr]
+
+// const islamic = {
+//     name: "Muslim",
+//     age: 30,
+//     isMarried: true,
+//     x:{
+//         a: 1,
+//         b: 2
+//     }
+// }
+
+// const newIslamic = {...islamic}
+
+// newIslamic.name = "Yusuf"
+
+// console.log(newIslamic);
+// console.log(islamic);
